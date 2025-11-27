cc21590de20ca4e118225ea41d50ba1ede9e4d6a
Sharjeel Khalid <sharjeelac@gmail.com>
2025-11-27 07:57:11 +0500

    updated API url to live server

--- client/src/url.js
+++ client/src/url.js
@@ -1,5 +1,6 @@
 // If we are on our computer, use localhost.
 // If we are online, we will update this variable later.
-export const baseUrl = "http://localhost:5000";
+export const baseUrl =
+  "https://maktabat-al-noor-crmbklgr2-sharjeel-khalids-projects.vercel.app";
 // Later we will change this line to:
 // export const baseUrl = "https://your-server-name.onrender.com";
