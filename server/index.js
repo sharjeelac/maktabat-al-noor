8684ec2b2ad7c4db8dcf4640d37e160b4d43f785
Sharjeel Khalid <sharjeelac@gmail.com>
2025-11-27 08:42:11 +0500

    Fixed Frontend 404 Refresh Issue

--- server/index.js
+++ server/index.js
@@ -24,16 +24,13 @@ mongoose
 
 // 3 :Middlewares (The GateKeepers)
 // Update CORS Configuration
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use(
+  cors({
+    origin: "*", // Allow ALL origins (Mobile, Localhost, Vercel)
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow all actions
+    allowedHeaders: ["Content-Type", "token"], // Allow our custom token header
+  })
+);
 app.use(express.json()); // Allow Backend to understand JSON
 
 // 4 : Simple Test Route
@@ -52,3 +49,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// --- ADD THIS LINE ---
+export default app;
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDNIARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
