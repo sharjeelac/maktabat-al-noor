721be5d9643e3f0808c7bc09c13832fe53bdb2ca
Sharjeel Khalid <sharjeelac@gmail.com>
2025-11-28 08:06:19 +0500

    baseurl updated

--- client/src/url.js
+++ client/src/url.js
@@ -1,6 +1,6 @@
 // If we are on our computer, use localhost.
 // If we are online, we will update this variable later.
-// export const baseUrl = "https://maktabat-al-noor-server.vercel.app";
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://maktabat-al-noor-server.vercel.app";
+// export const baseUrl = "http://localhost:5000";
 // Later we will change this line to:
 // export const baseUrl = "https://your-server-name.onrender.com";
